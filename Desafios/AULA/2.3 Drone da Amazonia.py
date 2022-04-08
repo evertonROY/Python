f197a0bd7849359aa3abe1f8098595725e64c867
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-04-07 22:39:55 -0300

    Create 2.3 Drone da Amazonia.py

---
+++ Desafios/AULA/2.3 Drone da Amazonia.py
@@ -0,0 +1,7 @@
+X, Y = [int(x) for x in input().split()]
+XA, YB = [int(x) for x in input().split()]
+
+if (X == XA and Y == YB):
+    print("Soltar pacote")
+else:
+    print('Nao soltar pacote')    
