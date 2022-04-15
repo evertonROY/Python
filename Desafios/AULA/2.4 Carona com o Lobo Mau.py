848f181518e5ef7016f28ff637581606b16512ed
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-04-15 17:41:36 -0300

    Update 2.4 Carona com o Lobo Mau.py

--- Desafios/AULA/2.4 Carona com o Lobo Mau.py
+++ Desafios/AULA/2.4 Carona com o Lobo Mau.py
@@ -4,9 +4,13 @@ C = int(N%3)
 V = int(N%3)
 L = int(N%3)
 
-#if N%3 == 1:
-    #C + 1
-#elif 
-#print(type(C))
-print(C)
+if N%3 == 1:
+    print(C + 1)
+elif N%3 == 2:
+    print(C + 1 & V + 1)
+else: 
+    print("opa") 
+
+
+
 
