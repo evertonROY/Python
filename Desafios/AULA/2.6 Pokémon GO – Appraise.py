3024a1695e4a5325b042c96347cd834906fa13bd
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-04-22 23:57:56 -0300

    Update 2.6 Pokémon GO – Appraise.py

---
+++ Desafios/AULA/2.6 Pokémon GO – Appraise.py
@@ -0,0 +1,13 @@
+A, D, P = [int(x) for x in input().split()]
+V = int(A + D + P)
+if V <= 50*110/100:
+    print("Seu pokemon nao fara progresso em batalhas")
+elif V <= 66*110/100:
+    print("Seu pokemon esta acima da media")
+elif V <= 79*110/100:
+    print("Seu pokemon certamente me chamou atencao")
+else:
+    print("Hum, parece que houve um erro")
+
+print(V)
+
