b871ed3b7c91624be6d311a4ef8974c576043e96
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-05-01 13:26:57 -0300

    .

--- Desafios/AULA/2.6 Pokémon GO – Appraise.py
+++ Desafios/AULA/2.6 Pokémon GO – Appraise.py
@@ -1,14 +1,16 @@
-A, D, P = [int(x) for x in input().split()]
-#A, D, P = map(int, input().split())
-V = int(A + D + P)
-if V <= 50*110/100:
+#A, D, P = [int(x) for x in input().split()]
+A, D, P = map(int, input().split())
+V = float(A + D + P)
+if V <= float((50*110)/100):
     print("Seu pokemon nao fara progresso em batalhas")
-elif V <= 66*110/100:
+elif V <= float((66*110)/100):
     print("Seu pokemon esta acima da media")
-elif V <= 79*110/100:
+elif V <= float((79*110)/100):
     print("Seu pokemon certamente me chamou atencao")
+elif V <= 110:
+    print("Seu pokemon e uma maravilha")
 else:
     print("Hum, parece que houve um erro")
 
-print(V)
+
 
