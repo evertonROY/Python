5b33c36fad37f8e09ffc726477d70637b14183ff
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-05-03 20:09:46 -0300

    .

---
+++ Desafios/Testes/Função.py
@@ -0,0 +1,12 @@
+from math import*
+# def é definição de funções 
+def  graus_para_radianos(a):
+    return a * pi / 180
+
+print(graus_para_radianos(0))
+print(graus_para_radianos(45))
+print(graus_para_radianos(90))
+
+def media(x, y):
+    return (x + y) / 2
+
