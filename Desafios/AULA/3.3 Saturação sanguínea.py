637bb15a2874ffad1beada10a7e0fec5ab1039cd
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-05-17 20:23:52 -0300

    .

--- Desafios/AULA/3.3 Saturação sanguínea.py
+++
@@ -1,5 +0,0 @@
-x = "Sair"
-print(x.lower())
-print(x.upper())
-
-print("asa" > "bola")
---
+++ Desafios/Testes/Listas.py
@@ -0,0 +1,11 @@
+x = "Sair"
+print(x.lower())
+print(x.upper())
+
+B = ["A", "B", "C"]
+Y = ["Logica de programação", B, "Olá mundo"]
+print(Y[1][2])
+print(type(Y))
+
+D = {1, 2, 2}
+print(type(D))
