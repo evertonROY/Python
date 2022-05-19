f59dc8ea774cab3e309904119bbe1d92326abf03
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-05-19 18:07:25 -0300

    Update 3.5 Vulcão.py

--- Desafios/AULA/3.5 Vulcão.py
+++ Desafios/AULA/3.5 Vulcão.py
@@ -3,11 +3,11 @@ D = int(input())
 P = (0)
 while D != 0:
     if D > P:
-     P = D
-     D = int(input())
+        P = D
+        D = int(input())
     else:
         D = int(input())
 if P > T:
-    print("ALARME") 
+    print("ALARME")
 else:
-    print("O Havai pode dormir tranquilo")           
+    print("O Havai pode dormir tranquilo")
