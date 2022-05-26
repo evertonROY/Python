6fbc513e55d03c7ca41643e772cb0e743f508e32
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-05-25 22:47:52 -0300

    .

---
+++ Desafios/Bigfat/bigfat.py
@@ -0,0 +1,5 @@
+print("Digite o primeiro numero!")
+A = int(input())
+print("Digite o segundo numero!")
+B = int(input())
+print("A soma de", A, "+", B, "=", int(A+B))
---
+++ Desafios/Testes/Listas e dicionario.py
@@ -0,0 +1,13 @@
+'''Listas'''
+Notas = [5.5, 4, 9, 10]
+print(Notas[2])  #9
+
+
+'''Dicionarios'''
+Moradores = {101:"joão", 102:"Maria", 105:"Pedro"}
+print(Moradores[101]) #João
+
+
+print(Moradores.values()) # mostra toda o dicionario 
+del Moradores[105] #deleta o morador pedro
+print(Moradores.values())
